4d15f56b4ef5bcc1a5b7809a3bb4be1d301543ab
Jono Cabrera <cjonathan193@gmail.com>
2023-12-23 18:49:40 +1100

    Added Image on Home. Responsive nav still W.I.P

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// This Script is still W.I.P
+// This will toggle the myTopNav menu in responsive view whenever the user taps on the icon
+
+const toggleBtn = document.querySelector("toggle_button")
+const toggleBtnIcon = document.querySelector("toggle_button i")
+const dropDownMenu = document.querySelector
